a1f1badc55fa2c21b2fb4ac39034093aa766ea3f
Anas <laangryanas@gmail.com>
2023-09-11 15:50:26 -0400

    feat: save liked songs

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,24 @@ import "./App.css";
 import Player from "./Player";
 import currentSong from "./songs/Millbrook - Overdrive (ft. Ellerslie) [NCS Release].mp3";
 
-const Song = {
-  name: "Overdrive (ft. Elleslie)",
-  artist: "Millbrook",
-};
+
+const demoPlaylist = [
+  {
+    name: "Overdrive (ft. Elleslie)",
+    artist: "Millbrook",
+    file: currentSong,
+    id: 1
+  },
+  {
+    name: "Overdrive (ft. Elleslie)",
+    artist: "Millbrook",  
+    file: currentSong,
+    id: 2
+  }
+]
 function App() {
   const [playerBig, setPlayerBig] = useState(false);
-  const [like, setLike] = useState(false);
+  const [like, setLike] = useState<number[]>([]);
   const [playerOpen, setPlayerOpen] = useState(false);
 
   function handleClick() {
@@ -17,7 +28,7 @@ function App() {
   }
 
   function handleClickOnList(e: React.MouseEvent<HTMLElement>) {
-    if ((e.target as HTMLDivElement).id === "first") {
+    if ((e.target as HTMLDivElement).className === "songlist") {
       setPlayerOpen(true);
       return handleClick();
     }
@@ -29,18 +40,27 @@ function App() {
         <p>apix music</p>
         <i className="fa-solid fa-music"></i>
       </div>
-      <div
+      <div id="playlist">
+      {demoPlaylist.map(song => {
+        return (
+          <div
         className="songlist"
-        id="first"
         onClick={(e) => handleClickOnList(e)}
       >
-        <p>{Song.name}</p>
-        <p>{Song.artist}</p>
+        <p>{song.name}</p>
+        <p>{song.artist}</p>
         <i
-          id="like-button"
-          onClick={() => setLike((prev) => !prev)}
-          className={!like ? "fa-regular fa-heart" : "fa-solid fa-heart pulse"}
+          onClick={() => setLike(prev => {
+            return like.includes(song.id) ?
+            prev.slice(0, like.indexOf(song.id)).concat(prev.slice(like.indexOf(song.id)+1)) 
+            : prev.concat(song.id)
+          })}
+          className={!like.includes(song.id) ? "fa-regular fa-heart like-button" : "fa-solid fa-heart pulse like-button"}
         ></i>
+      </div>
+        )
+      })
+      }
       </div>
       {playerOpen && (
         <Player onClick={handleClick} size={playerBig} song={currentSong} />
